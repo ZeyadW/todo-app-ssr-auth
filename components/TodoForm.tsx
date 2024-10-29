edafa68f2693673a9e205ff71ab22266435696d5
Zeyad <62406932+ZeyadW@users.noreply.github.com>
2024-10-29 17:37:04 +0300

    push

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,9 +1,6 @@
-import {createClient} from '@/lib/supabase/server';
 import {Button} from './ui/button';
 import {Input} from './ui/input';
-// import {addTodo} from '@/actions';
-import {revalidatePath} from 'next/cache';
-import {redirect} from 'next/navigation';
+import {addTodo} from '@/actions';
 
 interface TodoForm {
   title: string;
@@ -19,28 +16,7 @@ export const TodoForm: React.FC = () => {
   //   console.log(res);
   //   formData.reset();
   // };
-  async function addTodo(formData: FormData) {
-    'use server';
 
-    const title = formData.get('title');
-    const supabase = createClient();
-
-    const {
-      data: {user},
-      error: getUserError,
-    } = await supabase.auth.getUser();
-
-    if (getUserError) throw getUserError;
-
-    const {error: supabaseError} = await supabase
-      .from('todos')
-      .insert([{title: title, user_id: user?.id}]);
-
-    if (supabaseError) throw supabaseError;
-
-    revalidatePath('/', 'layout');
-    redirect('/?timestamp=' + Date.now());
-  }
   return (
     <form action={addTodo}>
       <div className="flex gap-2 ">
